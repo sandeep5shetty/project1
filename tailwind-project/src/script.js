2b2e0bdb6f16277d285646dd15e4e6afc3fe6f7e
sandeep5shetty <165358549+sandeep5shetty@users.noreply.github.com>
2025-01-25 13:11:45 +0530

    Add files via upload

--- tailwind-project/src/script.js
+++ tailwind-project/src/script.js
@@ -1,3 +1,16 @@
+// JavaScript for toggle button functionality
+const toggleButton = document.getElementById('toggleButton');
+const html = document.documentElement;
+
+toggleButton.addEventListener('click', () => {
+  html.classList.toggle('dark');
+});
+
+
+
+
+
+
 const navDialog=document.getElementById("nav-dialog");
 
 
@@ -37,15 +50,9 @@ function setupIntersectionObserver(element, isLTR, speed) {
 
 }
 
-const line1 = document.getElementById('line1');
-const line2 = document.getElementById('line2');
-const line3 = document.getElementById('line3');
 
 const line4 = document.getElementById('line4');
 
 
-setupIntersectionObserver(line1, true, 0.15);
-setupIntersectionObserver(line2, false, 0.15);
-setupIntersectionObserver(line3, true, 0.15);
 
 setupIntersectionObserver(line4, true, 1);
